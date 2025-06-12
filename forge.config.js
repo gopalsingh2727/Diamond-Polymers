9b0b5b9f7db2d2dee1301b5c2d3a6e9911475ce1
gopal singh <gopalsingh@gopals-MacBook-Air.local>
2025-06-12 20:56:52 +0530

    Update build config for Windows 10/11 support

---
+++ forge.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  packagerConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-dmg',
+      config: {
+        format: 'ULFO'
+      }
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin']
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {}
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {}
+    }
+  ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'gopalsingh2727',
+          name: 'Diamond-Polymers'
+        },
+        prerelease: false,
+        draft: true
+      }
+    }
+  ]
+};
